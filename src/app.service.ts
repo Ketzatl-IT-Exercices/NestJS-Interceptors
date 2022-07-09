b1dc25eeebca111708f5137d0f16964ac43ee2c1
Morgan <morgan.nott@hotmail.com>
2022-07-09 23:49:47 +0200

    Ajout d'une méthode générant des strings aléatoire

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,22 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const content: string = 'Salut les bilouttes !'
+    return `${content}`;
   }
+
+  makeString(): string {
+    let outString: string = '';
+    let inOptions: string = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 32; i++) {
+
+      outString += inOptions.charAt(Math.floor(Math.random() * inOptions.length));
+
+    }
+
+    return outString;
+  }
+
+  result: string = this.makeString();
 }
