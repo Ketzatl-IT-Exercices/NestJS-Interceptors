13ef4fb651fa106435cfe83a9881a80d03133df8
Morgan <morgan.nott@hotmail.com>
2022-07-10 00:29:08 +0200

    Implémentation d'un interceptor qui modifie la réponse et affiche un objet contenant la réponse initiale, après traitement, et sa longueur

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,14 @@ import {Controller, Get, UseInterceptors} from '@nestjs/common';
 import { AppService } from './app.service';
 import {MesureDurationInterceptor} from "./interceptors/mesure-duration.interceptor";
 import {LogsClientsInterceptor} from "./interceptors/logs-clients.interceptor";
+import {EnrichResponseInterceptor} from "./interceptors/enrich-response.interceptor";
 
 @Controller('api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseInterceptors(MesureDurationInterceptor, LogsClientsInterceptor)
+  @UseInterceptors(MesureDurationInterceptor, LogsClientsInterceptor, EnrichResponseInterceptor)
   getHello(): string {
     return this.appService.getHello();
   }
